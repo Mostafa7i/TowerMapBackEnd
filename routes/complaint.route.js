34210111bc16fa41db0803252db10282be63fc7d
Mostafa Mahmoud <mostasafi7@gmail.com>
2026-05-04 18:23:53 +0300

    add complaint route

--- routes/complaint.route.js
+++ routes/complaint.route.js
@@ -17,6 +17,13 @@ router.get("/my", verifyToken, getMyComplaints);
 
 // أدمن فقط
 router.get("/all", verifyToken, adminGuard, getAllComplaints);
+
+// تحديد الكل كمقروء (يجب أن يكون قبل /:id لتجنب التعارض)
+router.patch("/all/read", verifyToken, adminGuard, (req, res, next) => {
+  req.params.id = "all";
+  return markComplaintAsRead(req, res, next);
+});
+
 router.patch("/:id/status", verifyToken, adminGuard, updateComplaintStatus);
 router.patch("/:id/read", verifyToken, adminGuard, markComplaintAsRead);
 
